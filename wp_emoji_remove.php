db4f6f6d4eb21d241a459315b3a21d23621f4b00
myersg86 <myersg86@gmail.com>
2015-07-08 03:59:34 -0700

    Update wp_emoji_remove.php

--- wp_emoji_remove.php
+++ wp_emoji_remove.php
@@ -1,5 +1,5 @@
-
 // REMOVE WP EMOJI
+
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
